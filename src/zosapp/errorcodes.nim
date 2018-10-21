86372a274b377573df72b6dfbaad43f687d4372a
Ahmed T. Youssef <xmonader@gmail.com>
2018-10-21 13:27:09 +0200

    add error codes file

---
+++ src/zosapp/errorcodes.nim
@@ -0,0 +1,24 @@
+
+let cantCreateConfigDir* = 1
+let sshKeyNotFound* = 2
+let containerNotFound* = 3
+let vboxNotInstalled* = 4
+let unconfiguredZos* = 5
+let unknownCommand* = 6
+let sshToolsNotInstalled* = 7
+let portForwardExists* = 8
+let cantFindSshKeys* = 9
+let sshIsntEnabled* = 10
+let fileDoesntExist* = 11
+let unreachableZos* = 12
+
+# """
+# errorCodes
+# 1: can't create configdir
+# 2: sshkey not found
+# 3: container not found
+# 4: vbox not found
+# 5: unconfigured zos
+# 6: unknown command
+# 7: ssh tools not installed
+# """
