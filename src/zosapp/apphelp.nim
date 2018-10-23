aee43bd4b01bf820a21508a0a2ad52de91d10028
Ahmed T. Youssef <xmonader@gmail.com>
2018-10-23 11:03:33 +0200

    fix configure for first time

--- src/zosapp/apphelp.nim
+++ src/zosapp/apphelp.nim
@@ -12,7 +12,7 @@ To configure it to use a specific zosmachine
 let doc* = """
 Usage:
   zos init --name=<zosmachine> [--disksize=<disksize>] [--memory=<memorysize>] [--redisport=<redisport>] [--reset]
-  zos configure --name=<zosmachine> [--address=<address>] [--ports=<ports>] [--setdefault]
+  zos configure --name=<zosmachine> [--address=<address>] [--port=<port>] [--setdefault]
   zos remove --name=<zosmachine>
   zos ping
   zos showconfig
