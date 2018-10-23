4923923abf6bde37e6e8e538db3f69e595e4ac89
Dina-Abd-Elrahman <abdelrad@codescalers.com>
2018-10-23 14:22:43 +0200

    fix command

--- install_osx.sh
+++ install_osx.sh
@@ -3,5 +3,5 @@ brew install nim
 mkdir -p  ~/code/github;cd ~/code/github
 git clone https://github.com/threefoldtech/zos 
 cd zos
-nimble build -d:ssl --threads:on
+sudo nimble build -d:ssl --threads:on
 sudo cp zos /usr/local/bin
