f6cebd3be31b11d2cf6689adfcc05142960feceb
Dina-Abd-Elrahman <abdelrad@codescalers.com>
2018-10-23 14:21:18 +0200

    fix certain command

--- install_osx.sh
+++ install_osx.sh
@@ -3,5 +3,5 @@ brew install nim
 mkdir -p  ~/code/github;cd ~/code/github
 git clone https://github.com/threefoldtech/zos 
 cd zos
-nimble build -d:ssl
-cp zos /usr/local/bin
+nimble build -d:ssl --threads:on
+sudo cp zos /usr/local/bin
