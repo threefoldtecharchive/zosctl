d7c675054e2fdf7335e5ad4e89deaa68fbd81556
kristof de spiegeleer <despiegk@kristofs-MacBook-Pro.local>
2018-11-06 07:14:45 +0100

    bcdb acl

--- build.sh
+++ build.sh
@@ -1,15 +1,14 @@
 rm /usr/local/bin/zos
-export PATH=~/.nimble/bin:$PATH
- 
+# export NIM_LIB_PREFIX
+# export NIMBLE_DIR
+export PATH=$HOME/.nimble/bin:/usr/local/bin:/usr/bin:/bin
+export NIM_LIB_PREFIX=$HOME/.nimble/lib
+export NIMBLE_DIR=$HOME/.nimble
 set -ex
 
 export LDFLAGS="-L/usr/local/opt/openssl/lib"
 export CPPFLAGS="-I/usr/local/opt/openssl/include"
-export DYLD_LIBRARY_PATH=/usr/local/opt/openssl/lib
+export DYLD_LIBRARY_PATH="/usr/local/opt/openssl/lib"
 
-#brew install nim 
-#mkdir -p  ~/code/github;cd ~/code/github
-#git clone https://github.com/threefoldtech/zos 
-#cd zos
 sudo nimble build -d:ssl --threads:on
 sudo cp zos /usr/local/bin
