2126ce30532aa9537caaf9cc2805d1bb2b60be74
Dina-Abd-Elrahman <dena.elsayad@gmail.com>
2018-11-08 15:49:17 +0200

    add sudo before nim only

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ export NIM_LIB_PREFIX=$HOME/.nimble/lib
 export NIMBLE_DIR=$HOME/.nimble
 
 brew install openssl@1.1
-nim c -d:ssl  --dynlibOverride:ssl --dynlibOverride:crypto --threads:on --passC:'-I/usr/local/opt/openssl\@1.1/include/' --passL:'-lssl -lcrypto -lpcre' --passL:'-L/usr/local/opt/openssl\@1.1/lib/' src/zos.nim
+sudo nim c -d:ssl  --dynlibOverride:ssl --dynlibOverride:crypto --threads:on --passC:'-I/usr/local/opt/openssl\@1.1/include/' --passL:'-lssl -lcrypto -lpcre' --passL:'-L/usr/local/opt/openssl\@1.1/lib/' src/zos.nim
 
 sudo cp src/zos /usr/local/bin/zos
