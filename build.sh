6524767e6cab014537f3c9ed5482ef529d49141c
Dina-Abd-Elrahman <dena.elsayad@gmail.com>
2018-11-19 16:20:49 +0200

    redis must be at 0.1.1

--- build.sh
+++ build.sh
@@ -6,6 +6,10 @@ export NIM_LIB_PREFIX=$HOME/.nimble/lib
 export NIMBLE_DIR=$HOME/.nimble
 
 brew install openssl@1.1
+
+# redisclient & redisparser must be 0.1.1
+nimble uninstall redisclient
+nimble uninstall redisparser
 sudo nim c -d:ssl  --dynlibOverride:ssl --dynlibOverride:crypto --threads:on --passC:'-I/usr/local/opt/openssl\@1.1/include/' --passL:'-lssl -lcrypto -lpcre' --passL:'-L/usr/local/opt/openssl\@1.1/lib/' src/zos.nim
 
 # sudo nimble build -d:ssl --threads:on
