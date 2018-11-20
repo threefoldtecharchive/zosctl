d068239d6e88b2f8a0f4d9ee3ac7fc15fc08d997
kristof de spiegeleer <despiegk@kristofs-MacBook-Pro.local>
2018-11-20 14:19:38 +0100

    digitalmne

--- build.sh
+++ build.sh
@@ -5,9 +5,16 @@ export PATH=$HOME/.nimble/bin:/usr/local/bin:/usr/bin:/bin
 export NIM_LIB_PREFIX=$HOME/.nimble/lib
 export NIMBLE_DIR=$HOME/.nimble
 
+sudo nimble uninstall zos - y
+sudo nimble uninstall redisclient -y 
+sudo nimble uninstall redisparser -y
+
+nimble install redisclient@#head
+
 brew install openssl@1.1
 sudo nim c -d:ssl  --dynlibOverride:ssl --dynlibOverride:crypto --threads:on --passC:'-I/usr/local/opt/openssl\@1.1/include/' --passL:'-lssl -lcrypto -lpcre' --passL:'-L/usr/local/opt/openssl\@1.1/lib/' src/zos.nim
 
 # sudo nimble build -d:ssl --threads:on
 # sudo nimble build -d:openssl10 --threads:on
+
 sudo cp src/zos /usr/local/bin/zos
