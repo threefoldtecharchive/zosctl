20aee95c189fb9b0d3047cc71256ac55a35fab72
kristof de spiegeleer <>
2018-11-24 11:12:59 +0100

    ...

--- build.sh
+++ build.sh
@@ -1,24 +1,20 @@
-rm /usr/local/bin/zos
+rm -f /usr/local/bin/zos
 # export NIM_LIB_PREFIX
 # export NIMBLE_DIR
 export PATH=$HOME/.nimble/bin:/usr/local/bin:/usr/bin:/bin
 export NIM_LIB_PREFIX=$HOME/.nimble/lib
 export NIMBLE_DIR=$HOME/.nimble
 
-sudo nimble uninstall zos - y
-sudo nimble uninstall redisclient -y 
-sudo nimble uninstall redisparser -y
+sudo nimble uninstall zos -y > /dev/null 2>&1
+sudo nimble uninstall redisclient -y  > /dev/null 2>&1
+sudo nimble uninstall redisparser -y > /dev/null 2>&1
+rm -f /tmp/nimblecache/nimblepkg/*
 
+set -ex
 nimble install redisclient@#head
 
 brew install openssl@1.1
 
-# redisclient & redisparser must be 0.1.1
-nimble uninstall redisclient
-nimble uninstall redisparser
 sudo nim c -d:ssl  --dynlibOverride:ssl --dynlibOverride:crypto --threads:on --passC:'-I/usr/local/opt/openssl\@1.1/include/' --passL:'-lssl -lcrypto -lpcre' --passL:'-L/usr/local/opt/openssl\@1.1/lib/' src/zos.nim
-
-# sudo nimble build -d:ssl --threads:on
-# sudo nimble build -d:openssl10 --threads:on
 
 sudo cp src/zos /usr/local/bin/zos
