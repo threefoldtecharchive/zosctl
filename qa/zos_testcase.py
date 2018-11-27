304107de676a9012b2e1c5c8c32ca92e9b8a8e28
Dina-Abd-Elrahman <dena.elsayad@gmail.com>
2018-11-27 15:42:48 +0200

    add new changes

--- qa/zos_testcase.py
+++ qa/zos_testcase.py
@@ -3,6 +3,7 @@ import json
 import unittest
 import subprocess
 from jumpscale import j
+from netaddr import valid_ipv4
 from configparser import ConfigParser
 
 class SimpleTest(unittest.TestCase):
@@ -15,9 +16,8 @@ class SimpleTest(unittest.TestCase):
         """
         create VM instance and then create containers on top of it 
         """
-        self.default_init = subprocess.run(["./zos init --name=default_init --port=12345 --reset"], shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-        self.default_init_1 = subprocess.run(["./zos init --name=default_init_1 --port=123456 --reset"], shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-
+        default_init = subprocess.run(["./zos init --name=default_init --port=12345 --reset"], shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+        default_init_1 = subprocess.run(["./zos init --name=default_init_1 --port=123456 --reset"], shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
 
     def setUp(self):
         pass
@@ -64,9 +64,9 @@ class SimpleTest(unittest.TestCase):
         showactiveconfig =  subprocess.run(["./zos showactiveconfig"], shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE) 
         output_showactiveconfig = json.loads(showactiveconfig.stdout.decode())
         # need to check if the output contains (address, isvbox, port)
-        self.assertIn("address", output_showactiveconfig.keys(), msg=None)
-        self.assertIn("port", output_showactiveconfig.keys(), msg=None)
-        self.assertIn("isvbox", output_showactiveconfig.keys(), msg=None)
+        self.assertIn("address", output_showactiveconfig, msg=None)
+        self.assertIn("port", output_showactiveconfig, msg=None)
+        self.assertIn("isvbox", output_showactiveconfig, msg=None)
 
     def test_create_container(): 
         """
@@ -77,8 +77,8 @@ class SimpleTest(unittest.TestCase):
         container2 =  subprocess.run(["./zos container new --name=container2"], shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE) 
         test_con = j.clients.zos.get('test', data={'host':'127.0.0.1', 'port':'12345'})
         con_list = test_con.containers.list()
-        self.assertIn("2", con_list.keys(), msg="first container is created correctly")
-        self.assertIn("3", con_list.keys(), msg="second container is created correctly")
+        self.assertIn("2", con_list, msg="first container is created correctly")
+        self.assertIn("3", con_list, msg="second container is created correctly")
 
     def test_containers_list(self):     
         """
@@ -93,47 +93,18 @@ class SimpleTest(unittest.TestCase):
         self.assertIn("pid", output_container_list, msg=None)
         self.assertIn("ports", output_container_list, msg="container list command is working correctly")
 
-    def test_containers_info(self):
-        """
-            show summarized container info
-        """
-        container_info = subprocess.run(["./zos container info"], shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE) 
-        container_info_json = subprocess.run(["./zos container info --json"], shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)  
-        # showing info for certain container (need to check if it will passed to this function as list or string)
-        con_num = self.test_create_container()
-        container_info_ID = subprocess.run(["./zos container {} info".format(con_num)], shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE) 
-        # need to check container list
-
-    def test_containers_inspect(self):
-        """
-            inspect the current running 
-            container (showing full info)
-        """
-        con_num = self.test_create_container()
-        container_inspect = subprocess.run(["./zos container inspect"], shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE) 
-        container_inspect_ID = subprocess.run(["./zos container {} inspect".format(con_num)], shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-    
     def test_container_sshinfo(self):
         """
             show ssh info for certain container
         """
-        con_num = self.test_create_container()
         container_ssh_info = subprocess.run(["./zos container sshenable"], shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE) 
-        container_ssh_info_ID = subprocess.run(["./zos container {} sshenable".format(con_num)], shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE) 
+        username = container_ssh_info.stdout.decode().split("\n")[6].startswith("root") # check if it starts with root word 
+        self.assertTrue(username, True, msg=None)
+        ip = container_ssh_info.stdout.decode().split("\n")[6].split(" ")[0].split("@")[1]
+        ip_check = valid_ipv4(ip)
+        self.assertTrue(ip_check, True, msg=None)
 
-    def test_zerotier(self):   # may be not exist anymore 
-        """
-            zerotier info for certain container 
-        """
-        con_num = self.test_create_container()
-        container_zerotierinfo = subprocess.run(["./zos container {} zerotierinfo".format(con_num)], shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-        container_zerotierinfo_no_id = subprocess.run(["./zos container zerotierinfo"], shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-        container_zerotierlist = subprocess.run(["./zos container {} zerotierlist".format(con_num)], shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-        container_zerotierlist_no_id = subprocess.run(["./zos container zerotierlist"], shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-
-    def test_container_sshinfo():
-
-    def file_transfer(self):
+    def file_upload(self):
         """
             function to test upload & download for 
             files and directories from and to certain continer
@@ -150,7 +121,8 @@ class SimpleTest(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         # delete the vm instance
-        container_list = subprocess.run(["zos remove --name=default_init"], shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+        default_init_remove = subprocess.run(["zos remove --name=default_init"], shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+        default_init_1_remove = subprocess.run(["zos remove --name=default_init_1"], shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
 
     if __name__ == '__main__':
         unittest.main()
