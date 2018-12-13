dc011eb2ad762437b3c3d3b63665911863fb013d
Dina-Abd-Elrahman <dena.elsayad@gmail.com>
2018-12-13 18:01:43 +0200

    initialize jumpscale config manager

--- .travis_ci/prepare.sh
+++ .travis_ci/prepare.sh
@@ -9,9 +9,14 @@ export JSFULL=1
 
 curl https://raw.githubusercontent.com/threefoldtech/jumpscale_core/$JUMPSCALEBRANCH/install.sh > /tmp/install_jumpscale.sh;sudo -HE bash -c 'bash /tmp/install_jumpscale.sh'
 
-
 # create ssh key for jumpscale config manager
 mkdir -p ~/.ssh
 ssh-keygen -f ~/.ssh/id_rsa -P ''
 eval `ssh-agent -s`
 ssh-add ~/.ssh/id_rsa
+
+# initialize jumpscale config manager
+mkdir -p /opt/code/config_test
+git init /opt/code/config_test
+touch /opt/code/config_test/.jsconfig
+js_config init --silent --path /opt/code/config_test/ --key ~/.ssh/id_rsa
