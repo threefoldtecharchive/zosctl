5806771900d8b45992ba012bd1b850300e0b782b
xmonader <xmonader@gmail.com>
2018-12-17 15:21:29 +0200

    write logs into temp dir

--- src/commons/logger.nim
+++ src/commons/logger.nim
@@ -1,10 +1,11 @@
-import logging
+import logging, ospaths
+
 
 ## Global logging service for the application
 ## console logger is configured for levelinfo (lvlInfo) or higher
 ## file logger (zos.log) configured for all logging levels.
 
 var L* = newConsoleLogger(levelThreshold=lvlInfo)
-var fL* = newFileLogger("zos.log", levelThreshold=lvlAll, fmtStr = verboseFmtStr) ## logs saved in zos.log
+var fL* = newFileLogger(getTempDir() / "zos.log", levelThreshold=lvlAll, fmtStr = verboseFmtStr) ## logs saved in zos.log
 addHandler(L)
 addHandler(fL)
